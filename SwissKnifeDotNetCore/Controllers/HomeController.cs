32ef24e2c8ce9cee2608b1a5d7f4b34f4d86de61
koshyk <dimakoshyk@gmail.com>
2018-12-12 22:29:41 +0200

    [+] Swagger Swashbuckle

---
+++ SwissKnifeDotNetCore/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SwissKnifeDotNetCore.Controllers
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Swagger Docs
+    /// </summary>
+    //[SwaggerResponse(HttpStatusCode.NotFound, "Could not find the person", typeof(ErrorResponse))]
+    public class HomeController : Controller
+    {
+        // GET
+        /// <summary>
+        /// Return view
+        /// </summary>
+        /// <returns>View result</returns>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        /// <summary>
+        /// Get item
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Ok</returns>
+        [HttpGet]
+        public IActionResult GetItem(int id)
+        {
+            return Ok(new { item = id });
+        }
+
+        /// <summary>
+        /// Fake post
+        /// </summary>
+        /// <returns>Ok</returns>
+        [HttpPost]
+        public IActionResult PostItem()
+        {
+            return Ok();
+        }
+        
+        /// <summary>
+        /// Bad requests
+        /// </summary>
+        /// <returns>Bad</returns>
+        [HttpGet]
+        public IActionResult GetBadResult()
+        {
+            return BadRequest();
+        }
+    }
+}
--- SwissKnifeDotNetCore/Startup.cs
+++ SwissKnifeDotNetCore/Startup.cs
@@ -1,33 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace SwissKnifeDotNetCore
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                //.AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info()
+                    {
+                        Title = "Swashbuckle Sample API",
+                        Version = "v1",
+                        Description = "A sample API for testing Swashbuckle",
+                    }
+                );
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc();
 
-            app.Run(async (context) =>
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                c.RoutePrefix = ""; // serve the UI at root
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+                c.DisplayOperationId();
             });
         }
     }
