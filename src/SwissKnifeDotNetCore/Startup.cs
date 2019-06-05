fb74b8ffca375f1443c5992b13fe334b2bb49d72
Dmytro Koshyk <dkoshyk@netfully.com>
2019-06-05 14:13:06 +0300

    [~] comment run spa

--- src/SwissKnifeDotNetCore/Startup.cs
+++ src/SwissKnifeDotNetCore/Startup.cs
@@ -88,19 +88,19 @@ namespace SwissKnifeDotNetCore
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
+            //app.UseSpa(spa =>
+            //{
+            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
+            //    // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "../AngularApp";
-                spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
+            //    spa.Options.SourcePath = "../AngularApp";
+            //    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+            //    if (env.IsDevelopment())
+            //    {
+            //        spa.UseAngularCliServer(npmScript: "start");
                     
-                }
-            });
+            //    }
+            //});
         }
     }
 
