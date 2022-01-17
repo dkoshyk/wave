c02914d4acb48d749b1b73a5eab84cf23f2033f0
Dmytro Koshyk <dimakoshyk@gmail.com>
2022-01-17 12:31:27 +0200

    add with list api project

---
+++ src/WishList.API/Program.cs
@@ -0,0 +1,84 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddMediatR(typeof(Program));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+Task<string> PingCommand(string id, IMediator mediator) => mediator.Send(new Ping() { Name = id }); ;
+
+app.MapGet("/ping", PingCommand);
+
+app.MapControllers();
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class Ping : IRequest<string> 
+{ 
+    public string? Name { get; set; }
+}
+
+public class PingHandler : IRequestHandler<Ping, string>
+{
+    public Task<string> Handle(Ping request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult("Pong");
+    }
+}
+
+public class PingEndpoint 
+{ 
+    public string Route { get; set; }
+    public string Type { get; set; }
+    public IRequest Request { get; set; }
+
+    public Task<string> Handle(Ping request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult("Pong");
+    }
+}
+
+public class ApiMediator 
+{ 
+
+}
+
